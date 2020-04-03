84c5986582bc92450a24147e0ffc3cd39f6f6a02
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-04-03 18:53:40 +0530

    koushik commit

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  windowScrolled: boolean;
+  
+  constructor(public cookieService: CookieService, public router: Router, public activeroute: ActivatedRoute) { }
+
+
+  @HostListener("window:scroll", [])
+
+  onWindowScroll() {
+      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+      }
+      else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+  
+  scrollToTop() {
+      (function smoothscroll() {
+  
+          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  
+          if (currentScroll > 0) {
+              window.requestAnimationFrame(smoothscroll);
+              window.scrollTo(0, currentScroll - (currentScroll / 8));
+          }
+  
+      })();
+  }
 
   ngOnInit() {
   }
