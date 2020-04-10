e2943b94139210c9a660f1d5454b32b0d2cbde12
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-04-10 18:49:58 +0530

    mask landing backend

--- src/app/Components/order/order-edit/order-edit.component.ts
+++ src/app/Components/order/order-edit/order-edit.component.ts
@@ -8,19 +8,82 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import {CookieService} from 'ngx-cookie-service';
 
+
+export interface PeriodicElement {
+ 
+  productcode: number;
+  product: string;
+  qty: number;
+  unitprice: number;
+  total: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {productcode: 1, product: 'Hydrogen', qty: 1.0079, unitprice: 112, total: 112},
+  {productcode: 1, product: 'Hydrogen', qty: 1.0079, unitprice: 112, total: 112},
+  {productcode: 1, product: 'Hydrogen', qty: 1.0079, unitprice: 112, total: 112},
+  {productcode: 1, product: 'Hydrogen', qty: 1.0079, unitprice: 112, total: 112},
+];
+
+
+
+
+export interface TrElement { 
+  trid: string;
+  marcntID: string;
+  type: string;
+  datetime: string;
+  approval: number;
+  authorizationsode: number;
+}
+
+const TR_DATA: TrElement[] = [
+  {trid: '#1', marcntID: '#1', type: '1.0079', datetime: '12-04-2020', approval: 112, authorizationsode: 112},
+];
+
+
+export interface OrdAssVolumeElement { 
+  id: string;
+  type: string;
+  userID: string;
+  date: string;
+  amount: number;
+  voided: string;
+}
+
+const OrdAssVolume_DATA: OrdAssVolumeElement[] = [
+  {id: '#1', type: 'admin', userID: '#111', date: '12-04-2020', amount: 112, voided: '112'},
+];
+
 @Component({
   selector: 'app-order-edit',
   templateUrl: './order-edit.component.html',
   styleUrls: ['./order-edit.component.css']
 })
 export class OrderEditComponent implements OnInit {
+
+  displayedColumns = ['productcode', 'product', 'qty', 'unitprice', 'total'];
+  dataSource = ELEMENT_DATA;
+
+
+  displayedColumns1 = ['trid', 'marcntID', 'type', 'datetime', 'approval', 'authorizationsode'];
+  dataSource1 = TR_DATA;
+
+  displayedColumns2 = ['id', 'type', 'userID', 'date', 'amount', 'voided'];
+ 
+   dataSource2 = OrdAssVolume_DATA;
+
+
   public orderEditform: FormGroup;
   public orderData:any;
   public condition:any;
 
+  public header_text:any="Add Order";
+
   constructor(public activatedRoute:ActivatedRoute,public fb: FormBuilder) { 
 
     this.activatedRoute.params.subscribe(params => {
+      this.header_text = "Edit Order";
       if (params['_id'] != null) {
         this.condition = { id: params._id };
         this.activatedRoute.data.subscribe(resolveData => {
@@ -31,9 +94,20 @@ export class OrderEditComponent implements OnInit {
     })
     this.orderEditform = this.fb.group({
       id:null,
+      username: [null, Validators.required],      
       orderid: [null, Validators.required],
-      username: [null, Validators.required],
+      orderstatus: [null, Validators.required],
+      paymentstatus: [null, Validators.required],
       dateadded: [null, Validators.required],
+      ordersource: [null, Validators.required],
+      billingprofile: [null, Validators.required],
+      shippinginstruction: [null, Validators.required],
+      fulfillmentnotes: [null, Validators.required],
+      usercomments: [null, Validators.required],
+      admincomments: [null, Validators.required],
+      systemlog: [null, Validators.required],
+
+
       productname: [null, Validators.required],
       productquantity: [null, Validators.required],
       productprice: [null, Validators.required],
@@ -43,8 +117,10 @@ export class OrderEditComponent implements OnInit {
       total:[null, Validators.required]
     })
     this.orderEditform.patchValue({
-      orderid:this.orderData._id,
       username:this.orderData.user_info.firstname,
+      orderid:this.orderData._id,
+      
+
       productname:this.orderData.product_name,
       productquantity:this.orderData.product_qty,
       productprice:this.orderData.product_price,
@@ -55,7 +131,16 @@ export class OrderEditComponent implements OnInit {
 })
   }
 
+
+
   ngOnInit() {
+
+  
   }
+
+  // etTotalCost() {
+  //   return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  // }
+
 
 }
