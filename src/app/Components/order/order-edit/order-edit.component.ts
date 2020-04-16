a1f6642bebe23395b8976384f86ddda4d495d851
souravghatak-007 <sourav.influxiq@gmail.com>
2020-04-16 12:35:02 +0530

    push refund section

--- src/app/Components/order/order-edit/order-edit.component.ts
+++ src/app/Components/order/order-edit/order-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { HttpServiceService } from 'src/app/services/http-service.service';
 import * as moment from 'moment';
 import { from } from 'rxjs';
@@ -6,16 +6,16 @@ import { MetaService } from '@ngx-meta/core';
 import { FormGroup, FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
-import {CookieService} from 'ngx-cookie-service';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { CookieService } from 'ngx-cookie-service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiService } from '../../../api.service';
 
 export interface DialogData {
- alldata:any;
+  alldata: any;
 }
 
 export interface PeriodicElement {
- 
+
   productcode: number;
   product: string;
   qty: number;
@@ -26,7 +26,7 @@ export interface PeriodicElement {
 
 
 
-export interface TrElement { 
+export interface TrElement {
   trid: string;
   marcntID: string;
   type: string;
@@ -36,11 +36,11 @@ export interface TrElement {
 }
 
 const TR_DATA: TrElement[] = [
-  {trid: '#1', marcntID: '#1', type: '1.0079', datetime: '12-04-2020', approval: 112, authorizationsode: 112},
+  { trid: '#1', marcntID: '#1', type: '1.0079', datetime: '12-04-2020', approval: 112, authorizationsode: 112 },
 ];
 
 
-export interface OrdAssVolumeElement { 
+export interface OrdAssVolumeElement {
   id: string;
   type: string;
   userID: string;
@@ -50,7 +50,7 @@ export interface OrdAssVolumeElement {
 }
 
 const OrdAssVolume_DATA: OrdAssVolumeElement[] = [
-  {id: '#1', type: 'admin', userID: '#111', date: '12-04-2020', amount: 112, voided: '112'},
+  { id: '#1', type: 'admin', userID: '#111', date: '12-04-2020', amount: 112, voided: '112' },
 ];
 
 @Component({
@@ -59,15 +59,15 @@ const OrdAssVolume_DATA: OrdAssVolumeElement[] = [
   styleUrls: ['./order-edit.component.css']
 })
 export class OrderEditComponent implements OnInit {
-  public cookieUserallData:any=JSON.parse(this.cookieService.get('user_details'))
+  public cookieUserallData: any = JSON.parse(this.cookieService.get('user_details'))
 
-  displayedcontentsColumns = ['productcode','product', 'qty', 'unitprice', 'total'];
+  displayedcontentsColumns = ['productcode', 'product', 'qty', 'unitprice', 'total'];
 
 
   transactiondisplayedColumns = ['trid', 'marcntID', 'type', 'datetime', 'approval', 'authorizationsode'];
   transactiondataSource = TR_DATA;
 
-  orderassociateddisplayedColumns = ['id', 'type', 'userID', 'date', 'amount', 'voided']; 
+  orderassociateddisplayedColumns = ['id', 'type', 'userID', 'date', 'amount', 'voided'];
   orderassociateddataSource = OrdAssVolume_DATA;
 
   displayedSubTotalColumns = ['emptyFooter', 'emptyFooter', 'emptyFooter', 'subtotalTitle', 'subtotalAmount'];
@@ -76,13 +76,13 @@ export class OrderEditComponent implements OnInit {
 
 
   public orderEditform: FormGroup;
-  public orderData:any=[];
-  public condition:any;
-  public hideRequiredControl:any;
+  public orderData: any = [];
+  public condition: any;
+  public hideRequiredControl: any;
 
-  public header_text:any="Add Order";
+  public header_text: any = "Add Order";
 
-  constructor( public apiService: ApiService,public matSnackBar:MatSnackBar,public cookieService:CookieService,public activatedRoute:ActivatedRoute,public fb: FormBuilder,public meta: MetaService,public dialog: MatDialog) { 
+  constructor(public apiService: ApiService, public matSnackBar: MatSnackBar, public cookieService: CookieService, public activatedRoute: ActivatedRoute, public fb: FormBuilder, public meta: MetaService, public dialog: MatDialog) {
 
     this.meta.setTitle('Virus Medical Face Mask backend | Edit Order');
     this.meta.setTag('og:description', 'Virus Medical Face Mask backend to keep medical professionals safe and protected against harmful viruses, bacteria, and other critical circumstances, while also tending to their comfort.');
@@ -94,7 +94,7 @@ export class OrderEditComponent implements OnInit {
     this.meta.setTag('og:title', 'Virus Medical Face Mask backend | Edit Order');
     this.meta.setTag('twitter:title', 'Virus Medical Face Mask backend | Edit Order');
     this.meta.setTag('og:type', 'website');
-    this.meta.setTag('og:url','https://mask-landingpage-backend.influxiq.com/');    
+    this.meta.setTag('og:url', 'https://mask-landingpage-backend.influxiq.com/');
     this.meta.setTag('og:image', '../../assets/images/logo-fb.jpg');
     this.meta.setTag('twitter:image', '../../assets/images/logo-twitter.jpg');
 
@@ -132,44 +132,44 @@ export class OrderEditComponent implements OnInit {
       if (params['_id'] != null) {
         this.condition = { id: params._id };
         this.activatedRoute.data.subscribe(resolveData => {
-          setTimeout(()=>{    
+          setTimeout(() => {
             this.orderData = resolveData.orderData.res;
             console.log(this.orderData);
-       }, 500);
-         
+          }, 500);
+
         });
       }
     })
   }
+
   /**Void Trensection */
-  voidtransaction(){
+  voidtransaction() {
     console.warn("Void Transection here");
-    let data:any={
-      data:{
-      order_amount:this.orderData[0].product_total,
-      transactiontype:this.orderData[0].transactiontype,
-      transactionid:this.orderData[0].transactionid
+    let data: any = {
+      data: {
+        order_amount: this.orderData[0].product_total,
+        transactiontype: this.orderData[0].transactiontype,
+        transactionid: this.orderData[0].transactionid
       }
     }
-    
-    this.apiService.CustomRequest(data,'void-transaction').subscribe((res:any)=>{
+
+    this.apiService.CustomRequest(data, 'void-transaction').subscribe((res: any) => {
       console.log(res.resc.UAPIResponse.processorResponse);
       this.matSnackBar.open(res.resc.UAPIResponse.processorResponse, '', {
         duration: 3000
       });
-    
+
     });
   }
-/**refund function */
-refundOrder(){
-  console.warn("Refund Order");
-  this.openDialog();
-}
+  /**refund function */
+  refundOrder() {
+    this.openDialog();
+  }
   openDialog() {
     const dialogRef = this.dialog.open(RefundDailog, {
       panelClass: 'custom-modalbox',
       // width: '250px',
-       data: {data:this.orderData}
+      data: { data: this.orderData }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -177,18 +177,18 @@ refundOrder(){
     });
   }
   /**send email to oder id */
-  resendOrderReceipt(){
+  resendOrderReceipt() {
     console.log("resendOrderReceipt");
-    let data:any={
-      orderid:this.orderData[0]._id
+    let data: any = {
+      orderid: this.orderData[0]._id
     }
-    
-    this.apiService.CustomRequest(data,'resend-order-mail').subscribe((res:any)=>{
+
+    this.apiService.CustomRequest(data, 'resend-order-mail').subscribe((res: any) => {
       console.log(res);
       this.matSnackBar.open(res.msg, '', {
         duration: 3000
       });
-    
+
     });
   }
 
@@ -199,49 +199,64 @@ refundOrder(){
   templateUrl: 'refund.html',
 })
 export class RefundDailog {
-public checked = false;
-public refundamount:any=null;
-public dta:any=[];
+  public checked = false;
+  public refundamount: any = null;
+  public dta: any = [];
   constructor(
     public dialogRef: MatDialogRef<RefundDailog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData,public apiService: ApiService,public matSnackBar:MatSnackBar) {
-      this.dta=data;
-      //console.warn(this.dta.data[0]);
-    }
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, public apiService: ApiService, public matSnackBar: MatSnackBar) {
+    this.dta = data;
+    //console.warn(this.dta.data[0]);
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  refundFunction(){
-// if(this.refundamount==null && this.checked == true){
-//   console.warn(this.refundamount);
-// }else{
-//   console.warn(this.refundamount);
-// }
-    let data:any={
-      "data": {
-          "order_id":'OD'+this.dta.data[0]._id,
+  refundFunction() {
+    var data: any = {};
+    if (this.refundamount != null && this.checked == true) {
+      if (this.refundamount >= this.dta.data[0].product_total) {
+        console.log("error");
+        this.matSnackBar.open('Refund Amount must be less then Total Amount', '', {
+          duration: 3000
+        });
+      }
+      data = {
+        "data": {
+          "order_id": 'OD' + this.dta.data[0]._id,
+          "order_amount":this.refundamount ,
+          "transactionid": this.dta.data[0].transactionid,
+          "transactiontype": this.dta.data[0].transactiontype
+        }
+      }
+    } else {
+      data = {
+        "data": {
+          "order_id": 'OD' + this.dta.data[0]._id,
           "order_amount": this.dta.data[0].product_total,
           "transactionid": this.dta.data[0].transactionid,
           "transactiontype": this.dta.data[0].transactiontype
+        }
       }
-  }
-    // console.warn(data);
-    this.apiService.CustomRequest(data,'refund-transaction').subscribe((res:any)=>{
+    }
+
+    console.warn(data);
+    
+    this.apiService.CustomRequest(data, 'refund-transaction').subscribe((res: any) => {
       //console.log(res);
-      if(res.resc.UAPIResponse.processorResponse=="Approved"){
+      if (res.resc.UAPIResponse.processorResponse == "Approved") {
         this.matSnackBar.open(res.resc.UAPIResponse.processorResponse, '', {
           duration: 3000
         });
-      this.dialogRef.close();
-      }else{
+        this.dialogRef.close();
+      } else {
         this.matSnackBar.open(res.resc.UAPIResponse.statusMessage, '', {
           duration: 3000
         });
-      this.dialogRef.close();
+        this.dialogRef.close();
       }
-     
+
     });
   }
 
